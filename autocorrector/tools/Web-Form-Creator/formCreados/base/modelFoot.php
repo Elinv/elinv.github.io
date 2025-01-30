4a824ed0b64a8ede2bfcd4ad105d470c77d70e7e
elinv <elinv.elinv@gmail.com>
2025-01-30 18:01:55 -0300

    Si hay un ángel

--- autocorrector/tools/Web-Form-Creator/formCreados/base/modelFoot.php
+++
@@ -1,9 +0,0 @@
-            </div>
-        </div>
-    </div>
-    <footer class="container-fluid text-center">
-        <p>Elinv Form Builder</p>
-    </footer>
-</body>
-
-</html>
--- autocorrector/tools/titulos_bonitos/README.md
+++
@@ -1,5 +0,0 @@
-# Titulos-bonitos-Elinv
-
-Un generador de títulos en segundos. 
-
-Con todas las herramientas para su elaboración.
